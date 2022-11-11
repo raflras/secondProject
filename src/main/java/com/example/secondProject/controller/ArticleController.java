02a1ed3ccefd016f3a00004530d60344d8f450d6
raflras <raflras@naver.com>
2022-11-11 13:24:12 +0900

    221111_2

--- src/main/java/com/example/secondProject/controller/ArticleController.java
+++ src/main/java/com/example/secondProject/controller/ArticleController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @Slf4j
 public class ArticleController {
@@ -49,5 +51,15 @@ public class ArticleController {
         // 3 : 보여줄 페이지를 설정!
         return "articles/show";
 
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        // 1: 모든 Article 가져온다!
+        List<Article> articleEntityList = articleRepository.findAll();
+        // 2: 모든 Article 묶음을 뷰로 전달!
+        model.addAttribute("articleList", articleEntityList);
+        //3: 뷰 페이지를 설정!
+        return "articles/index";
     }
 }
--- src/main/java/com/example/secondProject/repository/ArticleRepository.java
+++ src/main/java/com/example/secondProject/repository/ArticleRepository.java
@@ -3,6 +3,9 @@ package com.example.secondProject.repository;
 import com.example.secondProject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+import java.util.ArrayList;
 
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
